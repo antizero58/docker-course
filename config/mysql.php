83f24db767bcac9375fa010fb0247da8d00b7ad4
antizero58 <antizero91@gmail.com>
2020-08-20 21:40:17 +0400

    docker-compose build for deploying

--- config/mysql.php
+++ config/mysql.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => yii\db\Connection::class,
-    'dsn' => 'mysql:host=mysql;dbname=mysql_db',
+    'dsn' => 'mysql:host=' . ($_ENV['MYSQL_HOST'] ?? 'mysql') . ';dbname=' . ($_ENV['MYSQL_DB'] ?? 'mysql_db'),
     'username' => 'root',
     'password' => 'password',
     'charset' => 'utf8',
